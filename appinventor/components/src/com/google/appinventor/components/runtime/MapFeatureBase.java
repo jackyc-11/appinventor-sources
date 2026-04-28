ShowPitekkate/appinventor-sources-ewpatton

Implement name lookup for components
Change-Id: I7f4bdc15e57880e6431f93c80ce9fd249b9e3040

appinventor/components/src/com/google/appinventor/components/runtime/MapFeatureBase.java
@@ -32,6 +32,7 @@ import org.osmdroid.util.GeoPoint;
 public abstract class MapFeatureBase implements MapFeature, HasStroke {
   protected MapFeatureContainer container = null;
   protected Map map = null;
+  private String name;
   private boolean visible = true;
   private int strokeColor = COLOR_BLACK;
   private float strokeOpacity = 1;
@@ -408,6 +409,21 @@ public abstract class MapFeatureBase implements MapFeature, HasStroke {
   @Override
   public HandlesEventDispatching getDispatchDelegate() {
     return map.getDispatchDelegate();
+  }
+
+  @Override
+  public void Name(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Gets the name of the %type%.
+   *
+   * @return the name of the component
+   */
+  @SimpleProperty
+  public String Name() {
+    return name;
   }
 
   @Override

jackyc-11/appinventor-sources

appinventor/components/src/com/google/appinventor/components/runtime/MapFeatureBase.java
@@ -34,6 +34,7 @@ public abstract class MapFeatureBase implements MapFeature, HasStroke {
 
   protected MapFeatureContainer container = null;
   protected Map map = null;
+  private String name;
   private boolean visible = true;
   private int strokeColor = COLOR_BLACK;
   private float strokeOpacity = 1;
@@ -112,6 +113,7 @@ public abstract class MapFeatureBase implements MapFeature, HasStroke {
   @Override
   public void setComponentName(String componentName) {
     this.componentName = componentName;
+    this.name = componentName;
   }
 
   public void setMap(MapFactory.MapFeatureContainer container) {
@@ -415,6 +417,16 @@ public abstract class MapFeatureBase implements MapFeature, HasStroke {
   @Override
   public HandlesEventDispatching getDispatchDelegate() {
     return map.getDispatchDelegate();
+  }
+
+  /**
+   * Gets the name of the %type%.
+   *
+   * @return the name of the component
+   */
+  @SimpleProperty
+  public String Name() {
+    return name;
   }
 
   @Override